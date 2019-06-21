7812caa4043d4a8c5b87faeca4e28d784bf93841
Tomáš Kallup <T.Kallup@gmail.com>
2019-06-21 17:12:51 +0200

    Updated dmenu cmd

--- config.h
+++ config.h
@@ -67,7 +67,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[]       = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[]       = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-l", 30, NULL };
 static const char *termcmd[]        = { "st", NULL };
 //static const char *ffcmd[]          = { "firefox", NULL };
 //static const char *voldcmd[]        = { "amixer", "sset", "Master", "-D", "pulse", "5%-", NULL };
